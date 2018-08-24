8bce3df7747fc9b84cf24b153794a829e6221f5e
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2018-08-24 10:22:53 +0800

    20180824

---
+++ app/src/main/java/com/example/tanstan/rxtest/RxMap.kt
@@ -0,0 +1,96 @@
+package com.example.tanstan.rxtest
+
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+fun useOfFlatMap() {
+    Observable.fromIterable(listOf(1,2,3,4,5,6))
+            .flatMap {
+                Observable.just(it)
+                        .map {
+                            if(it == 3 ) {
+                                Thread.sleep(1000)
+                            }
+                            it
+                        }
+                        .subscribeOn(Schedulers.newThread())
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Int> {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: Int) {
+                    System.out.println("on next: $t")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+            })
+}
+
+fun useOfConcatMap() {
+    Observable.fromIterable(listOf(1,2,3,4,5,6))
+            .concatMap {
+                Observable.just(it)
+                        .map {
+                            if(it == 3 ) {
+                                Thread.sleep(1000)
+                            }
+                            it
+                        }
+                        .subscribeOn(Schedulers.newThread())
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Int> {
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: Int) {
+                    System.out.println("on next: $t")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+            })
+}
+
+fun useOfSwtichMap() {
+    Observable.fromIterable(listOf(1,2,3,4,5,6))
+            .switchMap {
+                Observable.just(it)
+                        .subscribeOn(Schedulers.newThread())
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Int> {
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Int) {
+                    System.out.println("on next: $t")
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+            })
+}
--- app/src/main/java/com/example/tanstan/rxtest/RxObservable.kt
+++ app/src/main/java/com/example/tanstan/rxtest/RxObservable.kt
@@ -1,7 +1,7 @@
 package com.example.tanstan.rxtest
 
-import io.reactivex.Flowable
-import io.reactivex.Observable
+import io.reactivex.*
+import io.reactivex.disposables.Disposable
 import io.reactivex.subscribers.DisposableSubscriber
 import java.util.concurrent.TimeUnit
 
@@ -26,6 +26,88 @@ fun useOfFromCallable() {
                 override fun onNext(t: String) {
 
                     System.out.println(t)
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+            })
+}
+
+fun useOfFromRunnable() {
+    Maybe.fromRunnable<Unit> { println("Hello World!!") }
+            .subscribe(object : MaybeObserver<Unit> {
+                override fun onSuccess(t: Unit) {
+                    System.out.println("on success")
+                }
+
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+                    System.out.println(e.toString())
+                }
+
+            })
+}
+
+fun useOfMaybeFromCallable() {
+    Maybe.fromCallable { "Hello World!!!" }
+            .subscribe(object : MaybeObserver<String> {
+                override fun onSuccess(t: String) {
+                    System.out.println("on success: $t")
+                }
+
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+            })
+}
+
+fun useOfFromAction() {
+    Maybe.fromAction<Unit> { println("Hello World") }
+            .subscribe(object : MaybeObserver<Unit> {
+                override fun onSuccess(t: Unit) {
+                    System.out.println("on success")
+                }
+
+                override fun onComplete() {
+                    System.out.println("on complete")
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+}
+
+fun useOfSingle() {
+    Single.fromCallable { "Hello World" }
+            .subscribe(object : SingleObserver<String> {
+                override fun onSuccess(t: String) {
+                    println("on success: $t")
+                }
+
+                override fun onSubscribe(d: Disposable) {
 
                 }
 
